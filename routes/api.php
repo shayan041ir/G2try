aa9434d9c282a1d8ad1fb8e0ce4b9c738d70ffcf
shayan rezayi <shayanrezayi14@gmail.com>
2025-05-12 00:12:21 +0330

    [Enhance]: frist step

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,30 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+
+
+
+
+
+
+
+
+// whit out json
+// Route::get('/hello', function(){
+//     return Response('hello world');
+// });
+
+// with json and get
+// Route::get('/hello', function(){
+//     return Response()->json('hello world',200);
+// });
+
+// with json and post
+// Route::post('/hello', function(){
+//     return Response()->json('hello world',200);
+// });
